3e87cac38028de9123e34739d84bcae086f8e801
NishanthSpShetty <nishanthspshetty@gmail.com>
2022-08-09 13:09:00 +0530

    add -e to echo and update heredoc comment

--- setups/get_nvim.sh
+++ setups/get_nvim.sh
@@ -2,13 +2,13 @@ BLUE='\033[0;34m'
 
 RED='\033[0;31m'
 NC='\033[0m'
-echo "${BLUE}downloading latest nightly from repo ${NC}"
+echo -e "${BLUE}downloading latest nightly from repo ${NC}"
 
-curl -LOs /tmp/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+sudo curl -LOs /tmp/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
 
-echo "${BLUE}extracting tar${NC}"
-tar -xzf /tmp/nvim-linux64.tar.gz -C /tmp/
+echo -e "${BLUE}extracting tar${NC}"
+sudo tar -xzf /tmp/nvim-linux64.tar.gz -C /tmp/
 
-cp -rf /tmp/nvim-linux64/ /home/nishanth/installs/
-echo "${BLUE}copying binary${NC}"
-echo "${BLUE}done${NC}"
+echo -e "${BLUE}copying binary${NC}"
+sudo cp -rf /tmp/nvim-linux64/ /home/nishanth/installs/
+echo -e "${BLUE}done${NC}"
--- setups/init.sh
+++ setups/init.sh
@@ -16,27 +16,27 @@ cp -r ../vimrc/nvim-lsp/ ~/.config/nvim/
 
 cd
 sudo apt update
-echo "${GREEN}installing git ${NC}"
+echo -e "${GREEN}installing git ${NC}"
 sudo apt install -yq git
 
 # setup oh my zsh
-echo "${GREEN}installing zsh${NC}"
+echo -e "${GREEN}installing zsh${NC}"
 sudo apt install -yq zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # setup tmux 
 
-echo "${GREEN}installing tmux${NC}"
+echo -e "${GREEN}installing tmux${NC}"
 sudo apt install -yq tmux
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
 # add continuum and resurrect to tmux (save and resume session)
-echo "${GREEN}installing tmux plugins${NC}"
+echo -e "${GREEN}installing tmux plugins${NC}"
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/tmux-resurrect/
 git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/tmux-continuum/
-echo <<EOF >> .tmux/.tmux.conf  
+cat <<EOF >> ~/.tmux/.tmux.conf  
 run-shell ~/.tmux/tmux-resurrect/resurrect.tmux
 run-shell ~/.tmux/tmux-continuum/continuum.tmux
 set -g @resurrect-strategy-nvim 'session'
@@ -44,26 +44,26 @@ set -g @continuum-restore 'on'
 EOF
 
 ## install alacritty
-echo "${GREEN}installing alacritty${NC}"
+echo -e "${GREEN}installing alacritty${NC}"
 sudo add-apt-repository ppa:mmstick76/alacritty
 sudo apt install -yq alacritty
 
 # install nvim 
-echo "${GREEN}installing nvim...${NC}"
-bash get_nvim.sh
+echo -e "${GREEN}installing nvim...${NC}"
+bash ./get_nvim.sh
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # required tools 
-echo "${GREEN}installing golang${NC}"
+echo -e "${GREEN}installing golang${NC}"
 sudo apt install -yq golang 
 
-echo "${GREEN}installing rust${NC}"
+echo -e "${GREEN}installing rust${NC}"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-echo "${GREEN}installing starship${NC}"
+echo -e "${GREEN}installing starship${NC}"
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 # docker
-echo "${GREEN}installing docker${NC}"
+echo -e "${GREEN}installing docker${NC}"
 sudo apt install -yq \
     ca-certificates \
     curl \
@@ -79,9 +79,9 @@ echo \
 
 sudo apt update && sudo apt get install -yq docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-echo "${GREEN}installing nerd fonts${NC}"
+echo -e "${GREEN}installing nerd fonts${NC}"
 curl -LOs https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip
 unzip Agave.zip -d ~/.local/share/fonts/
 fc-cache -fv
 
-echo "${RED} Setup completed, hack away${NC}"
+echo -e "${RED} Setup completed, hack away${NC}"
