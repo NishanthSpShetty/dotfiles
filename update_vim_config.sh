8a49ded1fad72467883ebcf7bef28bb755de1922
NishanthSpShetty <nishanthspshetty@gmail.com>
2022-09-23 21:33:20 +0530

    add nvim conf copier from dotfiles to config path

---
+++ update_vim_config.sh
@@ -0,0 +1,4 @@
+echo "updating neovim config"
+cp nvim-config/init.vim ~/.config/nvim/init.vim
+cp nvim-config/lua/init.lua ~/.config/nvim/lua/init.lua
+cp -r nvim-config/lua/mine/* ~/.config/nvim/lua/mine
